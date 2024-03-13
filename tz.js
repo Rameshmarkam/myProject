cb47e437670b3bbfef0c77b651e18ac754c31873
Rameshmarkam <161702348+Rameshmarkam@users.noreply.github.com>
2024-03-13 10:11:09 +0530

    Add files via upload

---
+++ tz.js
@@ -0,0 +1,35 @@
+// let a = 'hi';
+// console.log(a);
+// {
+//     let a = 'hi ramesh bhaiya';
+//     console.log(a);
+// }
+// console.log(a);
+// // // find max number without using if condtion
+ const input = require('readline-sync');
+// // let a = input.questionInt('enter first no. : ')
+// // let b = input.questionInt('enter second no. : ')
+// // let max = (a > b) ? a:b;
+// // console.log('max:', max);
+// const input = require('readline-sync');
+// let N = input.questionInt('enter: ');
+// let i = 1,
+// m = Math.floor((N-1)/3);
+// while (i <= N)
+// {
+//     let str = '';
+//     if (i == 1)
+//     {
+//         str += ' '.repeat(m);
+//         str += 'OO';
+//         str += ' '
+//     }
+// }
+
+// const Arr = [2, 5, 3, 6, 7, 4];
+// Arr.sort();
+// console.log(Arr)
+
+const a = [1,2,3,4];
+const b = a.toSpliced(2,1);
+console.log(a,b);
